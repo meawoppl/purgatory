1201951a121e90a25dee49d3a94d50094a983a17
meawoppl <meawoppl@gmail.com>
2015-07-26 13:07:24 -0700

    Lint

--- dllextract.py
+++ dllextract.py
@@ -3,77 +3,79 @@ from io import BytesIO
 import struct
 from collections import OrderedDict, namedtuple
 
-printBar = lambda : print("=" * 80)
+printBar = lambda: print("=" * 80)
+
 
 def hexDump(flo):
-	startOffset = flo.tell()
-	bio = BytesIO(flo.read())
-	flo.seek(startOffset)
+    startOffset = flo.tell()
+    bio = BytesIO(flo.read())
+    flo.seek(startOffset)
 
-	readBytes = 0
-	printBar()
+    readBytes = 0
+    printBar()
 
-	breaks = 16
-	while True:
-		offset = startOffset + readBytes
-		if (offset % breaks) == 0:
-			print("\n%0.04i " % offset, end="")
+    breaks = 16
+    while True:
+        offset = startOffset + readBytes
+        if (offset % breaks) == 0:
+            print("\n%0.04i " % offset, end="")
 
-		seg = bio.read(1)
-		if len(seg) == 0:
-			break
+        seg = bio.read(1)
+        if len(seg) == 0:
+            break
 
-		inted = struct.unpack("B", seg)
+        inted = struct.unpack("B", seg)
 
-		print("%0.02x " % inted, end="")
-		if len(seg) < 1:
-			break
-		readBytes += len(seg)
+        print("%0.02x " % inted, end="")
+        if len(seg) < 1:
+            break
+        readBytes += len(seg)
 
-		if offset > 100:
-			break
+        if offset > 100:
+            break
 
-	print()
-	printBar()
+    print()
+    printBar()
+
 
 class StructExtract:
-	def __init__(self, nameOfStruct):
-		self.n = nameOfStruct
-		self.d = OrderedDict()
+    def __init__(self, nameOfStruct):
+        self.n = nameOfStruct
+        self.d = OrderedDict()
 
-	def addField(self, fmt, name, count=1):
-		self.d[name] = fmt, count
+    def addField(self, fmt, name, count=1):
+        self.d[name] = fmt, count
 
-	def structSize(self):
-		size = 0
-		for fmt, cnt in self.d.values():
-			if type(fmt) is str:
-				size += struct.calcsize(fmt * cnt)
-			elif type(fmt) is StructExtract:
-				size += fmt.structSize() * cnt
+    def structSize(self):
+        size = 0
+        for fmt, cnt in self.d.values():
+            if type(fmt) is str:
+                size += struct.calcsize(fmt * cnt)
+            elif type(fmt) is StructExtract:
+                size += fmt.structSize() * cnt
 
-		return size
+        return size
 
-	def readFromFLO(self, flo):
-		units = []
-		for fmt, cnt in self.d.values():
-			if type(fmt) is str:
-				fstr = fmt * cnt
-				unitSize = struct.calcsize(fstr)
-				bytez = flo.read(unitSize)
-				vals = struct.unpack(fstr, bytez)
-			elif type(fmt) is StructExtract:
-				vals = [fmt.readFromFLO(flo) for _ in range(cnt)]
-			else:
-				raise RuntimeError("Derp")
+    def readFromFLO(self, flo):
+        units = []
+        for fmt, cnt in self.d.values():
+            if type(fmt) is str:
+                fstr = fmt * cnt
+                unitSize = struct.calcsize(fstr)
+                bytez = flo.read(unitSize)
+                vals = struct.unpack(fstr, bytez)
+            elif type(fmt) is StructExtract:
+                vals = [fmt.readFromFLO(flo) for _ in range(cnt)]
+            else:
+                raise RuntimeError("Derp")
 
-			if len(vals) == 1:
-				units.append(vals[0])
-			else:
-				units.append(vals)
+            if len(vals) == 1:
+                units.append(vals[0])
+            else:
+                units.append(vals)
 
-		nt = namedtuple(self.n, self.d.keys())
-		return nt(*units)
+        nt = namedtuple(self.n, self.d.keys())
+        return nt(*units)
 
 
 ms_dos_header = StructExtract("MSDOS_Header")
@@ -207,121 +209,114 @@ image_export_directory.addField("I", "AddressOfNames")
 image_export_directory.addField("I", "AddressOfNameOrdinals")
 
 
-
 def readNTHeader(flo):
-	startOffset = flo.tell()
-	image_nt_headers = StructExtract("IMAGE_NT_HEADERS")
-	image_nt_headers.addField("4s", "Signature") # Actully DWORD, but this makes checking easier
-	image_nt_headers.addField(image_file_header, "FileHeader")
+    startOffset = flo.tell()
+    image_nt_headers = StructExtract("IMAGE_NT_HEADERS")
+    image_nt_headers.addField("4s", "Signature")   # Actully DWORD, but this makes checking easier
+    image_nt_headers.addField(image_file_header, "FileHeader")
 
-	# Read the next two bytes to compute the "magic"
-	flo.seek(startOffset + image_nt_headers.structSize())
-	magic = struct.unpack("H", flo.read(2))[0]
-	decoder = {0x10b: image_optional_header32, 0x20b: image_optional_header64}[magic]
-	flo.seek(startOffset)
+    # Read the next two bytes to compute the "magic"
+    flo.seek(startOffset + image_nt_headers.structSize())
+    magic = struct.unpack("H", flo.read(2))[0]
+    decoder = {0x10b: image_optional_header32, 0x20b: image_optional_header64}[magic]
+    flo.seek(startOffset)
 
-	# Use that magic to determin which header formatting to use, and read it
-	image_nt_headers.addField(decoder, "OptionalHeader")	
-	return image_nt_headers.readFromFLO(flo)
+    # Use that magic to determin which header formatting to use, and read it
+    image_nt_headers.addField(decoder, "OptionalHeader")
+    return image_nt_headers.readFromFLO(flo)
 
 
 def imageCharacteristicsDecode(char):
-	sectionHeaderFlags = {"CODE": 0x20, "IDATA": 0x40, "UDATA": 0x80,
-	                      "NOCACHE": 0x04000000, "NOPAGE": 0x08000000,
-	                      "SHARED": 0x10000000, "EXE": 0x20000000,
-	                      "READ": 0x40000000, "WRITE": 0x80000000}	
-	tags = []
-	for k, v in sorted(sectionHeaderFlags.items()):
-		if char & v:
-			tags.append(k)
+    sectionHeaderFlags = {"CODE": 0x20, "IDATA": 0x40, "UDATA": 0x80,
+                          "NOCACHE": 0x04000000, "NOPAGE": 0x08000000,
+                          "SHARED": 0x10000000, "EXE": 0x20000000,
+                          "READ": 0x40000000, "WRITE": 0x80000000}
+    tags = []
+    for k, v in sorted(sectionHeaderFlags.items()):
+        if char & v:
+            tags.append(k)
 
-	return " ".join(tags)
+    return " ".join(tags)
+
 
 def analyzeExports(flo, imageSectionHeader):
-	# Jump to my syection header
-	sectionAddress = imageSectionHeader.PointerToRawData
-	flo.seek(sectionAddress)
-	offsetAddress = imageSectionHeader.VirtualAddress - sectionAddress
+    # Jump to my syection header
+    sectionAddress = imageSectionHeader.PointerToRawData
+    flo.seek(sectionAddress)
+    offsetAddress = imageSectionHeader.VirtualAddress - sectionAddress
 
-	vaToFA = lambda va: va - offsetAddress
+    vaToFA = lambda va: va - offsetAddress
 
-	ied = image_export_directory.readFromFLO(flo)
+    ied = image_export_directory.readFromFLO(flo)
 
-	print("     >Exports Details: %i functions %i names" % (ied.NumberOfFunctions, ied.NumberOfNames))
-	# print(ied.AddressOfNames)
-	# print(ied.AddressOfFunctions)
+    print("     >Exports Details: %i functions %i names" % (ied.NumberOfFunctions, ied.NumberOfNames))
+    # print(ied.AddressOfNames)
+    # print(ied.AddressOfFunctions)
 
-	fileAddress = vaToFA(ied.AddressOfNames)
-	flo.seek(fileAddress)
+    fileAddress = vaToFA(ied.AddressOfNames)
+    flo.seek(fileAddress)
 
-	stringStarts = []
-	for i in range(ied.NumberOfNames):
-		stringPtr = struct.unpack("I", flo.read(4))[0]
-		fa = vaToFA(stringPtr)
-		stringStarts.append(fa)
+    stringStarts = []
+    for i in range(ied.NumberOfNames):
+        stringPtr = struct.unpack("I", flo.read(4))[0]
+        fa = vaToFA(stringPtr)
+        stringStarts.append(fa)
 
-	names = []
-	for startOffset in stringStarts:
-		flo.seek(startOffset)
+    names = []
+    for startOffset in stringStarts:
+        flo.seek(startOffset)
 
-		n = b""
-		while True:
-		 	n += flo.read(1)
-		 	if n[-1] == 0:
-		 		break
+        n = b""
+        while True:
+            n += flo.read(1)
+            if n[-1] == 0:
+                break
 
-		names.append(n[:-1].decode("ascii"))
+        names.append(n[:-1].decode("ascii"))
 
-
-	for idee, name in enumerate(names):
-		functionAddressAddress = vaToFA(ied.AddressOfFunctions + (4*idee))
-		flo.seek(functionAddressAddress)
-		adr = struct.unpack("I", flo.read(4))[0]
-		print("      > " + name, adr)
-
+    for idee, name in enumerate(names):
+        functionAddressAddress = vaToFA(ied.AddressOfFunctions + (4 * idee))
+        flo.seek(functionAddressAddress)
+        adr = struct.unpack("I", flo.read(4))[0]
+        print("      > " + name, adr)
 
 
+def extractDLL(pathToDLL):
+    # Open the file
+    f = open(pathToDLL, "rb")
 
-	1/0
+    # Skip over the msdos header
+    msdosHeader = ms_dos_header.readFromFLO(f)
+    dosHeaderEnd = msdosHeader.e_lfanew
+    f.seek(dosHeaderEnd)
 
-def extractDLL(pathToDLL):	
-	# Open the file
-	f = open(pathToDLL, "rb")
+    # Read the "optional" header
+    inth = readNTHeader(f)
+    assert inth.Signature == b"PE\x00\x00", "NT Header Magic (Should be PE00)"
 
-	# Skip over the msdos header
-	msdosHeader = ms_dos_header.readFromFLO(f)
-	dosHeaderEnd = msdosHeader.e_lfanew
-	f.seek(dosHeaderEnd)
+    print(inth.Signature)
 
-	# Read the "optional" header
-	inth = readNTHeader(f)
-	assert inth.Signature == b"PE\x00\x00", "NT Header Magic (Should be PE00)"
+    sectionCount = inth.FileHeader.NumberOfSections
 
-	print(inth.Signature)
+    sectionNames = []
+    print("Sections:")
+    print("=========")
+    for sectionHeaderNumber in range(sectionCount):
+        imageSection = image_section_header.readFromFLO(f)
+        sectionName = imageSection.Name.decode("ascii")
+        print(sectionName)
+        print("   > Start: ", imageSection.PointerToRawData)
+        print("   > Size: ", imageSection.SizeOfRawData)
+        print("   > Virtual Address", imageSection.VirtualAddress)
+        print("   > Flags: " + imageCharacteristicsDecode(imageSection.Characteristics))
 
-	sectionCount = inth.FileHeader.NumberOfSections
+        if sectionName.startswith(".edata"):
+            cursor = f.tell()
+            analyzeExports(f, imageSection)
+            f.seek(cursor)
 
-	sectionNames = []
-	print("Sections:")
-	print("=========")
-	for sectionHeaderNumber in range(sectionCount):
-		imageSection = image_section_header.readFromFLO(f)
-		sectionName = imageSection.Name.decode("ascii")
-		print(sectionName)
-		print("   > Start: ", imageSection.PointerToRawData)
-		print("   > Size: ", imageSection.SizeOfRawData)
-		print("   > Virtual Address", imageSection.VirtualAddress)
-		print("   > Flags: " + imageCharacteristicsDecode(imageSection.Characteristics))
-
-		if sectionName.startswith(".edata"):
-			cursor = f.tell()
-			analyzeExports(f, imageSection)
-			f.seek(cursor)
-
-		sectionNames.append(sectionName)
-	return sectionNames
-
-
+        sectionNames.append(sectionName)
+    return sectionNames
 
 
 print("Scanning Program Files:")
@@ -329,22 +324,22 @@ files = []
 sectionAccure = []
 folder = "/media/meawoppl/Blade/Program Files (x86)/"
 for root, dirs, fis in os.walk(folder):
-	for f in fis:
-		if f.endswith(".dll"):
-			fullPath = os.path.join(folder, root, f)
-			if os.path.islink(fullPath):
-				continue
-			files.append(fullPath)
+    for f in fis:
+        if f.endswith(".dll"):
+            fullPath = os.path.join(folder, root, f)
+            if os.path.islink(fullPath):
+                continue
+            files.append(fullPath)
 
 print("%i dll's found" % len(files))
 
 
 for n, fPath in enumerate(files):
-	print()
-	print("*" * 77)
-	print(fPath)
-	print("*" * 77)
-	print()
-	sectionAccure += extractDLL(fPath)
-	# if n > 5:
-	# 	break
+    print()
+    print("*" * 77)
+    print(fPath)
+    print("*" * 77)
+    print()
+    sectionAccure += extractDLL(fPath)
+    # if n > 5:
+    #   break
